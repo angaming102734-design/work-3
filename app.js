b3c6e51b0a84fe269c330b15e65a9390b905da70
ather ali <a.ngaming102734@gmail.com>
2026-03-06 04:10:08 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+// var text = 'What is the World War II';
+
+// for (var i = 0; i < text.length; i++) {
+//  if (text.slice(i, i + 12) === "World War II") {
+//  text = text.slice(0, 1) + "the Second World War" + text.slice(i + 12);
+//  }
+//  }
+
+// console.log(text);
+
+// var text = 'What is the World War II';
+
+// var chang = text.replace('What is the', 'I am');
+
+// console.log(chang);
+
+// var text = 'What is the ';
+
+// var car = 'This is a car';
+// for (var i = 0; i < car.length; i++){
+//     if (car.slice(i, i + 7) === "Lembogni") {
+//        car = car.slice(0, 1) + "Bugati" + car.slice(i + 6); 
+//     }  
+// }
+
+// console.log(car);
+// var text = 'What is the ';
+
+// var car = 'This is a car';
+// var chang = car.replace('This is a', 'I am a');
+// console.log(chang);
